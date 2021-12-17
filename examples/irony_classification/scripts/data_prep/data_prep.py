bc21174321d26cd3783326d1e977404f07da43cc
Samuel Brinkmann <samuel@brinkmann-familie.de>
2021-12-17 15:14:01 +0100

    Update data_prep.py
    - now it works

--- examples/irony_classification/scripts/data_prep/data_prep.py
+++ examples/irony_classification/scripts/data_prep/data_prep.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
+# path to raw data and paths for the preprocessed data
 data_path = "data/1_raw/data.csv"
 X_train_path = "data/2_processed/X_train.csv"
 y_train_path = "data/2_processed/y_train.csv"
@@ -28,7 +29,9 @@ def preprocess():
     """
     function to preprocess the raw data and save the processed data
     """
+
     print("preprocessing started...")
+    
     # read data
     print("... read data")
     df = pd.read_csv(data_path)
@@ -50,38 +53,26 @@ def preprocess():
     print("... clean tweets from hashtags")
     df["tweets_clean"] = df["tweets"].apply(clean_text)
 
-    # encode class labels
-    print("... encode class labels")
-    le = LabelEncoder()
-    df["class_encoded"] = le.fit_transform(df["class"])
-
     # splitting data into train and test
     print("... splitting data into test and train")
     X = df["tweets_clean"]
-    y = df["class_encoded"]
+    y = df["class"]
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
 
-    # vectorize text
-    print("... vectorize tweets")
-    cv = CountVectorizer(max_features=3000)
-
-    X_train_cv = cv.fit_transform(X_train).toarray()
-    X_test_cv = cv.transform(X_test).toarray()
-
     # convert the arrays back to pd.Dataframes
-    X_train_cv = pd.DataFrame(X_train_cv)
-    X_test_cv = pd.DataFrame(X_test_cv)
-    y_train = pd.DataFrame(X_train_cv)
-    y_test = pd.DataFrame(X_test_cv)
+    X_train = pd.DataFrame(X_train)
+    X_test = pd.DataFrame(X_test)
+    y_train = pd.DataFrame(y_train)
+    y_test = pd.DataFrame(y_test)
 
     # save processed data (index=False is very important otherwise the old indices will saved as new column)
     print("... save processed data")
-    X_train_cv.to_csv(X_train_path, index=False)
+    X_train.to_csv(X_train_path, index=False)
     y_train.to_csv(y_train_path, index=False)
-    X_test_cv.to_csv(X_test_path, index=False)
+    X_test.to_csv(X_test_path, index=False)
     y_test.to_csv(y_test_path, index=False)
     print("--> preprocessing completed")
 
